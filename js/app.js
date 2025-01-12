a1e4d712aeeea7fa83150cf593f517638746bab2
eyeonspringfield <eyeonspringfield@users.noreply.github.com>
2025-01-12 20:02:44 +0000

    deploy: b741cc93b24172ad616fa2d677ef19322940d552

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 "use strict";
 console.log("Script working!");
-//damn if this compiles i configured github actions correctly
+window.addEventListener("DOMContentLoaded", (event) => {
+    let currentPath = window.location.pathname;
+    let navLinks = document.querySelectorAll('#navbar-container .nav-element a');
+    navLinks.forEach(link => {
+        let linkHref = link.getAttribute('href');
+        if (linkHref && currentPath.includes(linkHref)) {
+            link.classList.add('active');
+        }
+    });
+});
