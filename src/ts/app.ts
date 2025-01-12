6ef76dd3fc5009f635b70ccc1bf6a8d41373c232
Csörgő Márk Ábrahám <h153041@stud.u-szeged.hu>
2025-01-12 23:58:47 +0100

    fixed navbar and changed navbar font

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,11 +3,20 @@ console.log("Script working!")
 window.addEventListener("DOMContentLoaded", (event: Event): void => {
     let currentPath: string = window.location.pathname;
     let navLinks: NodeListOf<Element> = document.querySelectorAll('#navbar-container .nav-element a');
+    let pathExists: boolean = false;
 
     navLinks.forEach(link => {
         let linkHref: string | null = link.getAttribute('href');
-        if(linkHref && currentPath.includes(linkHref)) {
+        if (linkHref && currentPath.includes(linkHref)) {
             link.classList.add('active');
+            pathExists = true;
         }
     })
+
+    if (!pathExists) {
+        let eye: HTMLElement | null = document.getElementById("eye");
+        if (eye){
+            eye.classList.add('active');
+        }
+    }
 })
