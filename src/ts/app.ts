b741cc93b24172ad616fa2d677ef19322940d552
Csörgő Márk Ábrahám <h153041@stud.u-szeged.hu>
2025-01-12 21:02:22 +0100

    Added landing page, about me projects and contact me pages, basic formatting on landing page, navbar

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,3 +1,13 @@
 console.log("Script working!")
 
-//damn if this compiles i configured github actions correctly
+window.addEventListener("DOMContentLoaded", (event: Event): void => {
+    let currentPath: string = window.location.pathname;
+    let navLinks: NodeListOf<Element> = document.querySelectorAll('#navbar-container .nav-element a');
+
+    navLinks.forEach(link => {
+        let linkHref: string | null = link.getAttribute('href');
+        if(linkHref && currentPath.includes(linkHref)) {
+            link.classList.add('active');
+        }
+    })
+})
