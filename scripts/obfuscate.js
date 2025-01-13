02149b3dd053fa44e0c8cbdfd16fa18fd48699b6
Csörgő Márk Ábrahám <h153041@stud.u-szeged.hu>
2025-01-13 18:04:20 +0100

    fix obfuscation

--- scripts/obfuscate.js
+++ scripts/obfuscate.js
@@ -5,12 +5,10 @@ const obfuscator = require('javascript-obfuscator');
 const inputDir = path.resolve(__dirname, '../dist/jsclean'); // Your JS source folder
 const outputDir = path.resolve(__dirname, '../dist/js'); // Output folder for obfuscated files
 
-// Ensure the output directory exists
 if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir, { recursive: true });
 }
 
-// Read all JS files from the input directory
 fs.readdir(inputDir, (err, files) => {
     if (err) {
         console.error('Error reading input directory:', err);
@@ -22,14 +20,12 @@ fs.readdir(inputDir, (err, files) => {
             const filePath = path.join(inputDir, file);
             const outputFilePath = path.join(outputDir, file);
 
-            // Read the file content
             fs.readFile(filePath, 'utf8', (readErr, content) => {
                 if (readErr) {
                     console.error(`Error reading file ${file}:`, readErr);
                     return;
                 }
 
-                // Obfuscate the file content
                 const obfuscated = obfuscator.obfuscate(content, {
                     compact: false,
                     controlFlowFlattening: true,
@@ -38,7 +34,6 @@ fs.readdir(inputDir, (err, files) => {
                     deadCodeInjectionThreshold: 0.4,
                 });
 
-                // Write the obfuscated content to the output file
                 fs.writeFile(outputFilePath, obfuscated.getObfuscatedCode(), (writeErr) => {
                     if (writeErr) {
                         console.error(`Error writing file ${outputFilePath}:`, writeErr);
